c1fb54d240034746e7a80a6845d024a607418388
Paul Adams <ghusiyy@gmail.com>
2023-08-18 23:59:31 -0700

    add simple logo

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,15 @@ import { setupNotes } from "./notes.ts";
 import { $el } from "./utils.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-   
-    <h1>NoteMosaic</h1>
-    <div id="notes">
-     
+  <div class="main">
+    <div class="mosaic-container">
+      <div class="mosaic-tile blue"></div>
+      <div class="mosaic-tile black"></div>
+      <div class="mosaic-tile black"></div>
+      <div class="mosaic-tile blue"></div>
     </div>
-    
+    <h1>NoteMosaic</h1>
+    <div id="notes"></div>
   </div>
 `;
 
